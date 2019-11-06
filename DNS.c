e8f22f56f1814c29b725c91b84a4ac114ef8d8d6
Saurabh Bhise <saurabhbhise16@live.com>
2019-11-06 11:09:55 +0530

    Update DNS.c

--- DNS.c
+++ DNS.c
@@ -1,5 +1,3 @@
-// C program to display hostname 
-// and IP address 
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
@@ -9,8 +7,6 @@
 #include <sys/socket.h> 
 #include <netinet/in.h> 
 #include <arpa/inet.h> 
-
-// Returns hostname for the local computer 
 void checkHostName(int hostname) 
 { 
 	if (hostname == -1) 
@@ -19,8 +15,6 @@ void checkHostName(int hostname)
 		exit(1); 
 	} 
 } 
-
-// Returns host information corresponding to host name 
 void checkHostEntry(struct hostent * hostentry) 
 { 
 	if (hostentry == NULL) 
@@ -29,9 +23,6 @@ void checkHostEntry(struct hostent * hostentry)
 		exit(1); 
 	} 
 } 
-
-// Converts space-delimited IPv4 addresses 
-// to dotted-decimal format 
 void checkIPbuffer(char *IPbuffer) 
 { 
 	if (NULL == IPbuffer) 
@@ -40,30 +31,17 @@ void checkIPbuffer(char *IPbuffer)
 		exit(1); 
 	} 
 } 
-
-// Driver code 
 int main() 
 { 
 	char hostbuffer[256]; 
 	char *IPbuffer; 
 	struct hostent *host_entry; 
 	int hostname; 
-
-	// To retrieve hostname 
 	hostname = gethostname(hostbuffer, sizeof(hostbuffer)); 
-	checkHostName(hostname); 
-
-	// To retrieve host information 
+	checkHostName(hostname);  
 	host_entry = gethostbyname(hostbuffer); 
 	checkHostEntry(host_entry); 
-
-	// To convert an Internet network 
-	// address into ASCII string 
-	IPbuffer = inet_ntoa(*((struct in_addr*) 
-						host_entry->h_addr_list[0])); 
-
+	IPbuffer = inet_ntoa(*((struct in_addr*) host_entry->h_addr_list[0]));
 	printf("Hostname: %s\n", hostbuffer); 
 	printf("Host IP: %s", IPbuffer); 
-
-	return 0; 
-} 
+	return 0; } 
